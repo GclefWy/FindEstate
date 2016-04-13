d712c232a6c22a9d3d5fab6655f12ef5687f7e83
Warren <wangyuan19850103@163.com>
2016-04-13 14:32:25 +0800

    FirstAdd

---
+++ AddressMatch/AddressMatch/Program.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+
+namespace AddressMatch
+{
+    class Program
+    {
+        static string getHttp(string url, string queryString)
+        {
+            //string queryString = "?";
+
+            //foreach (string key in httpContext.Request.QueryString.AllKeys)
+            //{
+            //queryString += key + "=" + httpContext.Request.QueryString[key] + "&";
+            //}
+
+            //queryString = queryString.Substring(0, queryString.Length - 1);
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url + queryString);
+            //Console.WriteLine(httpWebRequest.RequestUri);
+
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 20000;
+
+            //byte[] btBodys = Encoding.UTF8.GetBytes(body);
+            //httpWebRequest.ContentLength = btBodys.Length;
+            //httpWebRequest.GetRequestStream().Write(btBodys, 0, btBodys.Length);
+
+            string responseContent = "";
+            HttpWebResponse httpWebResponse;
+            StreamReader streamReader;
+            try
+            {
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                responseContent = streamReader.ReadToEnd();
+                httpWebResponse.Close();
+                streamReader.Close();
+            }
+            catch
+            {
+                responseContent = "";
+
+            }
+
+            return responseContent;
+        }
+
+        static string getAddrRoadName(string addr)
+        {
+            return Regex.Match(addr, @"(\w*市)*(\w*区)*(?'A'\w*?)(\d+|$)").Success ? Regex.Match(addr, @"(\w*市)*(\w*区)*(?'A'\w*?)(\d+|$)").Result(@"${A}") : "";
+        }
+
+        static string getAddrNum(string addr)
+        {
+            return Regex.Match(addr, @"(?'A'\d+)").Success ? Regex.Match(addr, @"(?'A'\d+)").Result(@"${A}") : "";
+        }
+
+        static void Main(string[] args)
+        {
+            string SConnDestination = ConfigurationManager.ConnectionStrings["SourceDBConn"].ConnectionString;
+            string SUserID = ConfigurationManager.AppSettings["SUserID"].ToString();
+            string SPSWD = ConfigurationManager.AppSettings["SPassword"].ToString();
+            string SRangeItem = ConfigurationManager.AppSettings["SRangeItem"].ToString();
+            string SRangeMin = ConfigurationManager.AppSettings["SRangeMin"].ToString();
+            string SRangeMax = ConfigurationManager.AppSettings["SRangeMax"].ToString();
+            string SRange = "(" + SRangeMin + "<=" + SRangeItem + " and " + SRangeItem + "<=" + SRangeMax + ")";
+
+            string TConnDestination = ConfigurationManager.ConnectionStrings["TargetDBConn"].ConnectionString;
+            string TUserID = ConfigurationManager.AppSettings["TUserID"].ToString();
+            string TPSWD = ConfigurationManager.AppSettings["TPassword"].ToString();
+
+            string ak = ConfigurationManager.AppSettings["ak"].ToString();
+            Random ra = new Random();
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = SConnDestination;
+                conn.Open();
+
+                using (SqlConnection conn2 = new SqlConnection())
+                {
+                    conn2.ConnectionString = TConnDestination;
+                    conn2.Open();
+
+                    using (SqlCommand command = conn.CreateCommand())
+                    {
+                        command.CommandText = "select tb_addr_id,tb_addr from TB_ADDR  with(nolock) where tb_geocoderAPI is null and " + SRange;
+                        //command.CommandText = "select tb_addr_id,tb_addr from TB_ADDR with(nolock) where tb_addr_id = 'A124F222-6293-4572-AD3C-765E207C867C'";
+                        
+                        using (SqlDataAdapter adp = new SqlDataAdapter(command))
+                        {
+                            DataSet ds = new DataSet();
+                            adp.Fill(ds);
+
+                            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                            {
+                                try
+                                {
+                                    string updateSQL = "";
+
+                                    string addrID = ds.Tables[0].Rows[i][0].ToString();
+                                    string addr = ds.Tables[0].Rows[i][1].ToString();
+
+                                    Console.WriteLine("");
+                                    Console.WriteLine(addrID + " : " + addr);
+
+                                    string geocoderURL = @"http://api.map.baidu.com/geocoder/v2/";
+                                    string geocoderQueryString = @"?address=" + addr + "&output=json&ak=" + ak;
+
+                                    string rtn = getHttp(geocoderURL, geocoderQueryString);
+
+                                    if (rtn.Length > 0)
+                                    {
+
+                                        JObject jo = JObject.Parse(rtn);
+
+                                        if (((int)jo["status"]) == 0)
+                                        {
+
+                                            string lng = (string)jo["result"]["location"]["lng"];
+                                            string lat = (string)jo["result"]["location"]["lat"];
+
+                                            Console.WriteLine("lng :" + lng + " , lat : " + lat);
+
+                                            int pagesize = 20;
+                                            string placeURL = @"http://api.map.baidu.com/place/v2/search";
+                                            string placeQueryString = @"?output=json&query=%E5%B0%8F%E5%8C%BA&page_size=" + pagesize.ToString() + "&page_num=0&scope=1&location=" + lat + "," + lng + "&radius=500&ak=" + ak;
+
+                                            string rtn2 = getHttp(placeURL, placeQueryString);
+
+                                            if (rtn2.Length > 0)
+                                            {
+                                                JObject jo2 = JObject.Parse(rtn2);
+
+                                                if (((int)jo2["status"]) == 0)
+                                                {
+
+                                                    int total = (int)jo2["total"];
+                                                    string unit = "";
+                                                    int unitConfidence = 0;
+                                                    for (int k = 0; k < (total < pagesize ? total : pagesize); k++)
+                                                    {
+                                                        if (k == 0)
+                                                        {
+                                                            unit = (string)jo2["results"][k]["name"];
+                                                            unitConfidence = 10;
+                                                        }
+                                                        if ((Regex.Match((string)jo2["results"][k]["address"], getAddrRoadName(addr)).Success) && (Regex.Match((string)jo2["results"][k]["address"], getAddrNum(addr)).Success))
+                                                        {
+                                                            unit = (string)jo2["results"][k]["name"];
+                                                            unitConfidence = 80;
+                                                            break;
+                                                        }
+                                                    }
+
+                                                    updateSQL += "tb_geocoderAPI = '" + rtn + "'";
+                                                    updateSQL += ",tb_lng=" + lng + ",tb_lat=" + lat;
+                                                    updateSQL += ",tb_placeAPI='" + rtn2 + "'";
+
+                                                    //using (SqlCommand command2 = conn2.CreateCommand())
+                                                    //{
+                                                    //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_geocoderAPI='" + rtn + "' where tb_addr_id='" + addrID + "'";
+                                                    //    command2.ExecuteNonQuery();
+
+                                                    //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_lng=" + lng + ",tb_lat=" + lat + " where tb_addr_id='" + addrID + "'";
+                                                    //    command2.ExecuteNonQuery();
+
+                                                    //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_placeAPI='" + rtn2 + "' where tb_addr_id='" + addrID + "'";
+                                                    //    command2.ExecuteNonQuery();
+                                                    //}
+
+                                                    if (unit.Length > 0)
+                                                    {
+
+                                                        Console.WriteLine(unit);
+
+                                                        string selectURL = "http://172.28.70.31:8080/two/collection1/select";
+                                                        string selecttQueryString = "?q=MultiName:" + unit + "&wt=json&indent=true&rows=5&fq=CityName:上海";
+
+                                                        string rtn3 = getHttp(selectURL, selecttQueryString);
+
+                                                        if (rtn3.Length > 0)
+                                                        {
+
+                                                            updateSQL += ",tb_unit='" + unit + "',tb_unit_confidence=" + unitConfidence.ToString();
+                                                            updateSQL += ",tb_selectAPI='" + rtn3 + "'";
+
+                                                            //using (SqlCommand command2 = conn2.CreateCommand())
+                                                            //{
+                                                            //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_unit='" + unit + "',tb_unit_confidence=" + unitConfidence.ToString() + " where tb_addr_id='" + addrID + "'";
+                                                            //    command2.ExecuteNonQuery();
+
+                                                            //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_selectAPI='" + rtn3 + "' where tb_addr_id='" + addrID + "'";
+                                                            //    command2.ExecuteNonQuery();
+
+                                                            //}
+
+
+                                                            JObject jo3 = JObject.Parse(rtn3);
+
+                                                            int rows = (int)jo3["responseHeader"]["params"]["rows"];
+                                                            string EstateID = "";
+                                                            int EstateIDConfidence = 0;
+                                                            try
+                                                            {
+                                                                for (int j = 0; j < rows; j++)
+                                                                {
+                                                                    if (j == 0)
+                                                                    {
+                                                                        EstateID = (string)jo3["response"]["docs"][j]["EstateID"];
+                                                                        EstateIDConfidence = 10;
+                                                                    }
+                                                                    if ((Regex.Match((string)jo3["response"]["docs"][j]["EstateName"], unit).Success) || ((Regex.Match((string)jo3["response"]["docs"][j]["Address"], getAddrRoadName(addr)).Success) && (Regex.Match((string)jo3["response"]["docs"][j]["Address"], getAddrNum(addr)).Success)))
+                                                                    {
+                                                                        EstateID = (string)jo3["response"]["docs"][j]["EstateID"];
+                                                                        EstateIDConfidence = 80;
+
+
+                                                                        break;
+                                                                    }
+
+                                                                }
+
+                                                                if (EstateID.Length > 0)
+                                                                {
+                                                                    updateSQL += ",tb_estate_id='" + EstateID + "', tb_estate_id_confidence=" + EstateIDConfidence.ToString();
+
+                                                                    //using (SqlCommand command2 = conn2.CreateCommand())
+                                                                    //{
+                                                                    //    command2.CommandText = "update TB_ADDR with(rowlock) set tb_estate_id='" + EstateID + "', tb_estate_id_confidence=" + EstateIDConfidence.ToString() + " where tb_addr_id='" + addrID + "'";
+                                                                    //    command2.ExecuteNonQuery();
+
+                                                                    //}
+
+                                                                    Console.WriteLine(EstateID);
+
+                                                                }
+
+                                                            }
+                                                            catch
+                                                            {
+
+                                                            }
+                                                        }
+                                                    }
+
+                                                }
+                                                else
+                                                {
+                                                    Console.WriteLine("get placeURL fail : " + rtn2);
+                                                    if (((int)jo["status"]) == 302)
+                                                    {
+                                                        Thread.Sleep(30 * 60 * 1000);
+                                                    }
+
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("get geocoderURL fail : " + rtn);
+                                            if (((int)jo["status"]) == 302)
+                                            {
+                                                Thread.Sleep(30 * 60 * 1000);
+                                            }
+                                        }
+                                    }
+
+                                    if (updateSQL.Length>0)
+                                    {
+                                        using (SqlCommand command2 = conn2.CreateCommand())
+                                        {
+                                            command2.CommandText = "update TB_ADDR with(rowlock) set "+updateSQL+" where tb_addr_id='" + addrID + "'";
+
+                                            Boolean sqlExecSuccess = false;
+                                            int sqlExecRetryCount = 5;
+
+                                            while (sqlExecRetryCount>0 && (!sqlExecSuccess))
+                                            {
+                                                try
+                                                {
+                                                    Console.WriteLine("update SQL start ...");
+                                                    command2.ExecuteNonQuery();
+                                                    sqlExecSuccess = true;
+                                                    Console.WriteLine("update SQL end");
+                                                }
+                                                catch
+                                                {
+                                                    Console.WriteLine("update SQL error");
+                                                    Console.WriteLine(command2.CommandText);
+                                                    Thread.Sleep(ra.Next(10)*200);
+                                                    Console.WriteLine("update SQL retry ("+ sqlExecRetryCount + ")");
+                                                }
+                                                finally
+                                                {
+                                                    sqlExecRetryCount--;
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AddressStandardization/AddressStandardization/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace AddressStandardization
+{
+    class Program
+    {
+        static string getAppSettingsByIndex(string prefix, int index)
+        {
+            if (ConfigurationManager.AppSettings[prefix + index.ToString()]==null)
+            {
+                return null;
+            } else
+            {
+                return ConfigurationManager.AppSettings[prefix + index.ToString()].ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string SConnDestination = ConfigurationManager.ConnectionStrings["SourceDBConn"].ConnectionString;
+            string SUserID = ConfigurationManager.AppSettings["SUserID"].ToString();
+            string SPSWD = ConfigurationManager.AppSettings["SPassword"].ToString();
+
+            string TConnDestination = ConfigurationManager.ConnectionStrings["TargetDBConn"].ConnectionString;
+            string TUserID = ConfigurationManager.AppSettings["TUserID"].ToString();
+            string TPSWD = ConfigurationManager.AppSettings["TPassword"].ToString();
+
+            string City = ConfigurationManager.AppSettings["City"].ToString();
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = SConnDestination;
+                conn.Open();
+
+                using (SqlConnection conn2 = new SqlConnection())
+                {
+                    conn2.ConnectionString = TConnDestination;
+                    conn2.Open();
+
+                    using (SqlCommand command = conn.CreateCommand())
+                    {
+                        command.CommandText = "select id,contact_address from TB_MC_CUSTOMER_GFZ where id>5512294 and live_city='" + City + "'";
+
+                        using (SqlDataAdapter adp = new SqlDataAdapter(command))
+                        {
+                            DataSet ds = new DataSet();
+                            adp.Fill(ds);
+
+                            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                            {
+                                string customerID = ds.Tables[0].Rows[i][0].ToString();
+                                string addrS = ds.Tables[0].Rows[i][1].ToString();
+
+                                Console.WriteLine(customerID+" : "+addrS);
+
+                                string addrT = addrS;
+                                int index = 1;
+                                while (getAppSettingsByIndex("Search_",index) != null)
+                                {
+                                    addrT = Regex.Match(addrT, getAppSettingsByIndex("Search_", index)).Success ? Regex.Match(addrT, getAppSettingsByIndex("Search_", index)).Result(getAppSettingsByIndex("Replace_", index)) : addrT;
+
+                                    index++;
+                                }
+
+                                if (addrT.Trim().Length>0)
+                                {
+                                    Guid addrID = Guid.NewGuid();
+                                    using (SqlCommand command2 = conn2.CreateCommand())
+                                    {
+                                        command2.CommandText = "select tb_addr_id from TB_ADDR where tb_addr='" + addrT + "'";
+
+                                        using (SqlDataAdapter adp2 = new SqlDataAdapter(command2))
+                                        {
+                                            DataSet ds2 = new DataSet();
+                                            adp2.Fill(ds2);
+
+                                            if (ds2.Tables[0].Rows.Count > 0)
+                                            {
+                                                addrID = new Guid(ds2.Tables[0].Rows[0][0].ToString());
+                                            }
+                                            else
+                                            {
+                                                string insertSQL2 = "insert into TB_ADDR (tb_addr_id, tb_addr) values('" + addrID + "','" + addrT + "')";
+                                                command2.CommandText = insertSQL2;
+                                                command2.ExecuteNonQuery();
+
+                                            }
+
+                                        }
+
+                                    }
+
+                                    Console.WriteLine(addrID + " : " + addrT);
+
+                                    string insertSQL1 = "insert into TB_CUSTOMER_ADDR (customer_id, addr_id) values(" + customerID + ",'" + addrID + "')";
+
+                                    using (SqlCommand command2 = conn2.CreateCommand())
+                                    {
+                                        command2.CommandText = insertSQL1;
+                                        command2.ExecuteNonQuery();
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
