4158e7972135b7666e442bda3745a51497ae7851
Warren <wangyuan19850103@163.com>
2016-04-15 14:20:44 +0800

    111

--- AddressMatch/AddressMatchMain/Program.cs
+++ AddressMatch/AddressMatchMain/Program.cs
@@ -32,7 +32,6 @@ namespace AddressMatchMain
             DataSet DSToT5 = new DataSet();
 
             DataSet DSFromT = new DataSet();
-            //DataSet DSFromT2 = new DataSet();
 
             string sql1 = "select tb_addr_id,tb_addr,tb_addr_id_num from [TB_ADDR_20160415]  with(nolock) where tb_addr_id_num<=200";
              DSToT1 = SimpleDataHelper.Query(SimpleDataHelper.MSConnectionString, sql1);
@@ -49,33 +48,33 @@ namespace AddressMatchMain
             string sql5 = "select tb_addr_id,tb_addr,tb_addr_id_num  from [TB_ADDR_20160415]  with(nolock) where tb_addr_id_num>500 and tb_addr_id_num<=600";
             DSToT5 = SimpleDataHelper.Query(SimpleDataHelper.MSConnectionString, sql5);
 
-
+            
             Console.WriteLine("===== 异步回调 AsyncInvoke =====");
            
 
             var t1 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("bkVoYqmhhPWZavSf59pYsWgo1kvPGDXh", "上海",DSToT1));
 
-            //var t2 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("G9cddRpBtdh8gtVMT6gSnS1b9reuCKHs", "上海",DSToT2));
+            var t2 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("G9cddRpBtdh8gtVMT6gSnS1b9reuCKHs", "上海", DSToT2));
 
-            //var t3 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("HdA7n8YuTHzfdLLYgwWdf4LaZWRgcmeG", "上海", DSToT3));
-            //var t4 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("Y5DStlEzvc0a228OeecY8I3Dqm1CzoZb", "上海", DSToT4));
-            //var t5 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("bRbMPYGGgFPNqmxy80rDt3Gh", "上海", DSToT5));
+            var t3 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("HdA7n8YuTHzfdLLYgwWdf4LaZWRgcmeG", "上海", DSToT3));
+            var t4 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("Y5DStlEzvc0a228OeecY8I3Dqm1CzoZb", "上海", DSToT4));
+            var t5 = Task.Factory.StartNew(() => AddressMatch.Program.AddrMatch("bRbMPYGGgFPNqmxy80rDt3Gh", "上海", DSToT5));
 
             Console.WriteLine("等待中。。。");
 
-            //Task.WaitAll(t1, t2,t3,t4,t5);
-            Task.WaitAll(t1);
+            Task.WaitAll(t1, t2,t3,t4,t5);
+            //Task.WaitAll(t1);
 
 
             DSFromT = t1.Result;
 
-            //DSFromT.Merge(t2.Result);
+            DSFromT.Merge(t2.Result);
 
-            //DSFromT.Merge(t3.Result);
+            DSFromT.Merge(t3.Result);
 
-            //DSFromT.Merge(t4.Result);
+            DSFromT.Merge(t4.Result);
 
-            //DSFromT.Merge(t5.Result);
+            DSFromT.Merge(t5.Result);
 
             string tbaddridnumSet = "";
 
@@ -85,7 +84,7 @@ namespace AddressMatchMain
 
             }
 
-            string sqldelete = "delete [TB_ADDR] where tb_addr_id_num in ("+tbaddridnumSet.Substring(0,tbaddridnumSet.Length-1)+")";
+            string sqldelete = "delete [TB_ADDR_20160415] where tb_addr_id_num in (" + tbaddridnumSet.Substring(0,tbaddridnumSet.Length-1)+")";
 
             SimpleDataHelper.Excsql(SimpleDataHelper.MSConnectionString, sqldelete);
 
